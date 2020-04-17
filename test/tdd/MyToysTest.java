ee46b5ea20555c7a8aab1f29c0af52643cc1b59d
minhnhat-leon <nhatbpmse130488@fpt.edu.vn>
2020-04-17 08:48:41 +0700

    7th push: green color

--- test/tdd/MyToysTest.java
+++ test/tdd/MyToysTest.java
@@ -22,7 +22,7 @@ public class MyToysTest {
     
     @Test
     public void testSuccessfullCases() {
-        assertEquals(10, cF(0));
+        assertEquals(1, cF(0));
         assertEquals(2, cF(2));
         assertEquals(6, cF(3));
         assertEquals(24, cF(4));
