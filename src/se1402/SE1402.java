fde2af8f6a81bfb8601fd2b8aea72b278ceee18e
minhnhat-leon <nhatbpmse130488@fpt.edu.vn>
2020-11-18 21:51:15 +0700

    "test cnfig"

--- src/se1402/SE1402.java
+++ src/se1402/SE1402.java
@@ -15,7 +15,7 @@ public class SE1402 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("VUi ghê");
+        System.out.println("VUi");
     }
     
 }
